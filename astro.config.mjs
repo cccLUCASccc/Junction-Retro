2a8a2575ac1ba41e2441c06443b088a232da367a
LucasC <lucascloes@gmail.com>
2026-04-19 16:21:41 +0200

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import svelte from '@astrojs/svelte';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://junction-retro.com',
+
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [svelte()]
+  integrations: [svelte(), sitemap()]
 });
